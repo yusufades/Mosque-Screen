48524ce39fff87958348c30154334c0332e56b73
Yusuf Ades <y.ades@optergy.com>
2022-05-28 01:31:43 +1000

    add icv logo

--- src/_components/logo/logo.js
+++ src/_components/logo/logo.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './logo.css';
 import AppConfig from '../app-config/app-config';
+import ICV_Logo from '../../_assets/ICV_logo.png';
 
 class Logo extends Component {
   constructor(props) {
     super(props);
     var _appConfig = new AppConfig();
     this.state = {
+      logo_enabled: _appConfig.get('enable_logo').toLowerCase() === 'true',
       logo: _appConfig.get('Logo_URL')
     };
   }
@@ -14,7 +16,11 @@ class Logo extends Component {
   render() {
     return (
       <div className="LogoWrapper d-none d-lg-block">
-        <img src={this.state.logo} alt="" />
+        {this.state.logo_enabled ? (
+          <img src={this.state.logo || ICV_Logo} alt="" />
+        ) : (
+          <></>
+        )}
       </div>
     );
   }
